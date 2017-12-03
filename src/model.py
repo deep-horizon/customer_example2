c8a6ee0b018fd751265419842c469021879e4a20
Ben Whittle <benwhittle31@gmail.com>
2017-12-02 22:07:39 -0800

    first commit

---
+++ src/model.py
@@ -0,0 +1,17 @@
+from definitions import model_path
+
+
+def train():
+  print('Training...')
+
+  # Training will produce the model file
+  with open(model_path, 'w+') as f:
+    f.write('the good stuff')
+
+
+def test():
+  print('Testing!')
+
+
+def predict(data):
+  return {'prediction': 'some-value2'}
