346d09daeba386cafcca057cfb868d1555c6bd95
Ben Whittle <benwhittle31@gmail.com>
2017-12-19 10:43:02 -0500

    asdf

--- src/dataset.py
+++ src/dataset.py
@@ -2,4 +2,4 @@ def prepro(data):
   if data:
     print('Preprocessing dataset with keys: {}'.format(data[0].keys()))
   else:
-    print('Preprocessing dataset...')
+    print('Preprocessing dataset`...')
